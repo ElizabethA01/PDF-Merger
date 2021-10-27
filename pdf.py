3ffa672688866a725d499479ab1c08fa447ce6a6
Elizabeth Adejumo <a.adejumo13@gmail.com>
2021-10-27 11:29:29 +0100

    created a shortened version of the code

--- pdf.py
+++ pdf.py
@@ -4,13 +4,12 @@ import sys
 inputs = sys.argv[1:]
 watermark_file = 'wtr.pdf'
 output = 'super.pdf'
-merged = 'wtr-super.pdf'
+merged = 'watermakred-output.pdf'
 
 def pdf_combiner(pdf_list):
     #merge pages in the file
     merger = PyPDF2.PdfFileMerger()
     for pdf in pdf_list:
-        print(pdf)
         #add pages
         merger.append(pdf)
     #creates a new document
@@ -22,25 +21,26 @@ def watermarker(combined_file, watermarker):
         #read content of the combined file
         input_pdf = PyPDF2.PdfFileReader(input_file)     
         #read content of the watermark
-        watermark_pdf = PyPDF2.PdfFileReader(watermark_file)
-        #get first page of the watermark PDF
-        watermark_page = watermark_pdf.getPage(0)
+        watermark_pdf = PyPDF2.PdfFileReader(watermark_file)        
         #create a pdf writer object for the output file
-        pdf_writer = PyPDF2.PdfFileWriter()
+        output = PyPDF2.PdfFileWriter()
 
         for i in range(input_pdf.getNumPages()):
             #get pages of the combined file
             combined_pages = input_pdf.getPage(i)
 
+            #get first page of the watermark PDF
+            watermark_page = watermark_pdf.getPage(0)
+
             #merge the two pages
             combined_pages.mergePage(watermark_page)
 
             #add page
-            pdf_writer.addPage(combined_pages)
+            output.addPage(combined_pages)
 
             with open(merged, 'wb') as merged_file:
                 #write the watermarked file to the new file 
-                pdf_writer.write(merged_file)
+                output.write(merged_file)
 
 
 pdf_combiner(inputs)
